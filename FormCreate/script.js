992d1d89b47f93a356c102867c45601c3d0969a2
sky-lu <honglin.lu.sr@gmail.com>
2020-04-02 19:59:27 -0400

    login + form create

---
+++ FormCreate/script.js
@@ -0,0 +1,28 @@
+﻿//json
+
+var products = [];
+
+
+//Register Function
+function Register() {
+
+    //Obtain the data
+    var name = document.getElementById("name").value;
+    var brand = document.getElementById("brand").value;
+    var value = document.getElementById("value").value;
+
+    //Register with json
+    products.push
+    ({
+        "name": name,
+        "brand": brand,
+        "value": value,
+
+    })
+
+    //clean fields
+    clean();
+
+    //List the products
+
+}
