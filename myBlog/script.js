a453ea28ffc0a011fbd1c1c36f517aa3d94f24f7
sky-lu <honglin.lu.sr@gmail.com>
2020-04-19 20:37:17 -0400

    myBlog-local storage\jquery

--- myBlog/script.js
+++ myBlog/script.js
@@ -1,59 +1,108 @@
 ﻿
-    //窗口载入事件
-    window.onload = (function () {
-        //验证每一个input值是否为空，以及是否符合表单项业务规则（长度，类型，邮箱规则，手机规则），如果不符合规则，弹框告诉用户，并返回false
-
-        //如果全部验证通过，则弹出提交成功框，用户点击了弹框中的确认按钮，跳转到首页
-        document.getElementById("contactform").onsubmit = function (event) {
+        window.onload = (function () {
+       
+        
+        document.getElementById("send").onclick = function (event) {
             
             var name = document.getElementById("name");
+            var errorname = document.getElementById("errorname");
             if (name.value == "") {
-                document.getElementById("errorname").innerText = "* Please enter your name!"
+                errorname.innerText = "* Please enter your name!";
                 name.focus()
                 return false
 
             }
             var email = document.getElementById("email");
+            var erroremail = document.getElementById("erroremail");
             if (email.value == "") {
-                document.getElementById("erroremail").innerText = "* Please enter your email!";
+                erroremail.innerText = "* Please enter your email!";
                 email.focus();
                 return false;
             }
             var regExp = /\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*/;
             if (!regExp.test(email.value)) {
-                document.getElementById("erroremail").innerText = "* Please enter the correct email address!";
+                erroremail.innerText = "* Please enter the correct email address!";
                 email.value = "";
                 email.focus();
                 return false;
             }
             var phone = document.getElementById("phone");
+            var errorphone = document.getElementById("errorphone");
             if (phone.value == "") {
-                document.getElementById("errorphone").innerText = "* Please enter your phone number!";
+                errorphone.innerText = "* Please enter your phone number!";
                 phone.focus();
                 return false;
             }
-            var myreg = /^(((13[0-9]{1})|(14[0-9]{1})|(17[0-9]{1})|(15[0-3]{1})|(15[4-9]{1})|(18[0-9]{1})|(199))+\d{8})$/;
+            var myreg = /^[0-9]+.?[0-9]*/;
             if (!myreg.test(phone.value)) {
-                document.getElementById("errorphone").innerText = "* Please enter the correct phone number!";
+                errorphone.innerText = "* Please enter the correct phone number!";
                 phone.value = "";
                 phone.focus();
                 return false;
             }
             var message = document.getElementById("message");
+            var errormessage = document.getElementById("errormessage");
             if (message.value == "") {
-                document.getElementById("errormessage").innerText = "* Please enter your message!"
+                errormessage.innerText = "* Please enter your message!"
                 message.focus();
                 event.preventDefault()
 
                 return false;
             }
 
-            //document.getElementById("biglayer").style.display = "block";
-            //document.getElementById("biglayer").style.width = window.innerWidth;
-            //document.getElementById("biglayer").style.height = window.innerHeight;
+            setStorage();
+
+            name.value = email.value = phone.value = message.value = "";
+            errorname.innerText = erroremail.innerText = errorphone.innerText = errormessage.innerText = "";
+            var box = document.getElementById("box");
+            box.style.width = document.body.clientWidth + "px"
+            box.style.height = document.body.clientHeight + "px"
+            box.style.display = "block"
+            showQuestions();
+            return false
+
         }
 
     });
 
-  
-   
+function ok() {
+    var box = document.getElementById("box");
+    box.style.display = "none"
+}
+$(document).ready(function () {
+    $("#icon-swing").click(function () {
+        $("#information-box").toggle();
+        showQuestions();
+    });
+});
+
+var questions = []
+
+function setStorage() {
+    if (localStorage.getItem('message') != null) {
+        questions = JSON.parse(localStorage.getItem('message'));
+    }
+    questions.push({
+        "message": $("#message").val()
+    })
+    localStorage.setItem("message", JSON.stringify(questions));
+
+}
+
+function getStorage() {
+    questions = JSON.parse(localStorage.getItem('message')) || [];
+    return questions;
+}
+
+function showQuestions() {
+    questions = getStorage();
+    var quest_list = ""
+    if (questions.length != 0) {
+        for (var i = 0; i < questions.length; i++) {
+            quest_list += "<li>" + questions[i].message + "</li>";
+
+        }
+        $(".info_list").html(quest_list);
+    }
+    
+}
